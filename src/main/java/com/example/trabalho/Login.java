2f5df5f1fb1b241d487a8edb9c90001decdd45fe
Jose Miguel Mendes <up201906869@up.pt>
2022-10-28 20:07:53 +0100

    First Commit

---
+++ src/main/java/com/example/trabalho/Login.java
@@ -0,0 +1,29 @@
+package com.example.trabalho;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class Login {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private PasswordField password;
+
+}
