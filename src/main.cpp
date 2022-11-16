7d3470f63122633990dab3b75dae07b03cedb5ed
Jose Miguel Mendes <up201906869@up.pt>
2022-11-16 22:33:22 +0000

    Teste servo#4

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,26 @@
 #include <RP2040_PWM.h>
 
 
-#define MOTOR_1 2
-#define MOTOR_2 3
+#define MOTOR 3
 
 RP2040_PWM* PWM_Instance;
-RP2040_PWM* PWM_Instance1;
+
 
 void setup() {
   // put your setup code here, to run once:
   
-  PWM_Instance = new RP2040_PWM(MOTOR_1,50,10);
-  PWM_Instance1 = new RP2040_PWM(MOTOR_2,50,10);
- 
+  PWM_Instance = new RP2040_PWM(MOTOR,100,26);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    
-    if (PWM_Instance){
-      PWM_Instance1-> setPWM(MOTOR_2,50,10);
-      PWM_Instance -> setPWM(MOTOR_1,50,10);
-    }
+
+    PWM_Instance -> setPWM(MOTOR,100,4);
+    Serial.print("aa");
     delay(1000);
-    PWM_Instance1 -> setPWM(MOTOR_2, 50, 5);
-    PWM_Instance -> setPWM(MOTOR_1,50,10);
+
+    PWM_Instance -> setPWM(MOTOR,100,26);
     delay(1000);
     
 }
