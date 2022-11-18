ad52fb310743964b04267a226933d7e4d7fda557
José Mendes <61622582+zemendes1@users.noreply.github.com>
2022-11-18 09:32:19 +0000

    Syntax Changes

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,10 @@
 #define MOTOR_7 16
 #define MOTOR_8 17
 
+
+#define Freq 150
+#define Ograus 6
+
 RP2040_PWM *PWM_Instance_1A;
 RP2040_PWM *PWM_Instance_1B;
 RP2040_PWM *PWM_Instance_6A;
@@ -26,22 +30,21 @@ void setup()
 {
   // put your setup code here, to run once:
 
-  PWM_Instance_1A = new RP2040_PWM(MOTOR_1, 150, 6);
-  PWM_Instance_1B = new RP2040_PWM(MOTOR_2, 150, 6);
+  PWM_Instance_1A = new RP2040_PWM(MOTOR_1, Freq, Ograus);
+  PWM_Instance_1B = new RP2040_PWM(MOTOR_2, Freq, Ograus);
 
-  PWM_Instance_6A = new RP2040_PWM(MOTOR_3, 150, 6);
-  PWM_Instance_6B = new RP2040_PWM(MOTOR_4, 150, 6);
+  PWM_Instance_6A = new RP2040_PWM(MOTOR_3, Freq, Ograus);
+  PWM_Instance_6B = new RP2040_PWM(MOTOR_4, Freq, Ograus);
 
-  PWM_Instance_7A = new RP2040_PWM(MOTOR_5, 150, 6);
-  PWM_Instance_7B = new RP2040_PWM(MOTOR_6, 150, 6);
+  PWM_Instance_7A = new RP2040_PWM(MOTOR_5, Freq, Ograus);
+  PWM_Instance_7B = new RP2040_PWM(MOTOR_6, Freq, Ograus);
 
-  PWM_Instance_0A = new RP2040_PWM(MOTOR_7, 150, 6);
-  PWM_Instance_0B = new RP2040_PWM(MOTOR_8, 150, 6);
+  PWM_Instance_0A = new RP2040_PWM(MOTOR_7, Freq, Ograus);
+  PWM_Instance_0B = new RP2040_PWM(MOTOR_8, Freq, Ograus);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
 
   for (int i = 1; i < 9; i++)
   {
@@ -49,7 +52,6 @@ void loop()
   }
   delay(1000);
 
-  // PWM_Instance -> setPWM(MOTOR,150,36);
   for (int i = 1; i < 9; i++)
   {
     servo_angle(180, i);
@@ -59,30 +61,33 @@ void loop()
 
 void servo_angle(int angle, int motor)
 {
+
+  if(angle < 0 || angle > 180){ return; }
+
   int value = 0;
-  value = 0.1667 * angle + 6;
+  value = (1/6) * angle + 6;
 
-  if (motor == 1)
-    PWM_Instance_1A->setPWM(MOTOR_1, 150, value);
+  if (motor == 1){
+    PWM_Instance_1A->setPWM(MOTOR_1, Freq, value);}
 
-  else if (motor == 2)
-    PWM_Instance_1B->setPWM(MOTOR_2, 150, value);
+  else if (motor == 2){
+    PWM_Instance_1B->setPWM(MOTOR_2, Freq, value);}
 
-  else if (motor == 3)
-    PWM_Instance_6A->setPWM(MOTOR_3, 150, value);
+  else if (motor == 3){
+    PWM_Instance_6A->setPWM(MOTOR_3, Freq, value);}
 
-  else if (motor == 4)
-    PWM_Instance_6B->setPWM(MOTOR_4, 150, value);
+  else if (motor == 4){
+    PWM_Instance_6B->setPWM(MOTOR_4, Freq, value);}
 
-  else if (motor == 5)
-    PWM_Instance_7A->setPWM(MOTOR_5, 150, value);
+  else if (motor == 5){
+    PWM_Instance_7A->setPWM(MOTOR_5, Freq, value);}
 
-  else if (motor == 6)
-    PWM_Instance_7B->setPWM(MOTOR_6, 150, value);
+  else if (motor == 6){
+    PWM_Instance_7B->setPWM(MOTOR_6, Freq, value);}
 
-  else if (motor == 7)
-    PWM_Instance_0A->setPWM(MOTOR_7, 150, value);
+  else if (motor == 7){
+    PWM_Instance_0A->setPWM(MOTOR_7, Freq, value);}
 
-  else if (motor == 8)
-    PWM_Instance_0B->setPWM(MOTOR_8, 150, value);
+  else if (motor == 8){
+    PWM_Instance_0B->setPWM(MOTOR_8, Freq, value);}
 }
