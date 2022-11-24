fca3b7c2df4bbdd8fd93a669bda1638c193eb2d6
Diogo Vilela <diogoagoncalves2001@gmail.com>
2022-11-24 10:23:24 +0000

    Pessimo Commit

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,10 @@ RP2040_PWM *PWM_Instance_1A;
 RP2040_PWM *PWM_Instance_1B;
 RP2040_PWM *PWM_Instance_6A;
 RP2040_PWM *PWM_Instance_6B;
-RP2040_PWM *PWM_Instance_7A;
+/*RP2040_PWM *PWM_Instance_7A;
 RP2040_PWM *PWM_Instance_7B;
 RP2040_PWM *PWM_Instance_0A;
-RP2040_PWM *PWM_Instance_0B;
+RP2040_PWM *PWM_Instance_0B;*/
 
 void servo_angle(int angle, int motor);
 
@@ -36,25 +36,25 @@ void setup()
   PWM_Instance_6A = new RP2040_PWM(MOTOR_3, Freq, Ograus);
   PWM_Instance_6B = new RP2040_PWM(MOTOR_4, Freq, Ograus);
 
-  PWM_Instance_7A = new RP2040_PWM(MOTOR_5, Freq, Ograus);
+  /*PWM_Instance_7A = new RP2040_PWM(MOTOR_5, Freq, Ograus);
   PWM_Instance_7B = new RP2040_PWM(MOTOR_6, Freq, Ograus);
 
   PWM_Instance_0A = new RP2040_PWM(MOTOR_7, Freq, Ograus);
-  PWM_Instance_0B = new RP2040_PWM(MOTOR_8, Freq, Ograus);
+  PWM_Instance_0B = new RP2040_PWM(MOTOR_8, Freq, Ograus);*/
 }
 
 void loop()
 {
 
-  for (int i = 1; i < 9; i++)
+  for (int i = 1; i < 5; i++)
   {
-    servo_angle(0, i);
+    servo_angle(5, i);
   }
   delay(1000);
-
-  for (int i = 1; i < 9; i++)
+  
+  for (int i = 1; i < 5; i++)
   {
-    servo_angle(180, i);
+    servo_angle(175, i);
   }
   delay(1000);
 }
@@ -64,8 +64,8 @@ void servo_angle(int angle, int motor)
 
   if(angle < 0 || angle > 180){ return; }
 
-  int value = 0;
-  value = (1/6) * angle + 6;
+  float value=0;
+  value = (0.16666666666666667) * angle + 6;
 
   if (motor == 1){
     PWM_Instance_1A->setPWM(MOTOR_1, Freq, value);}
@@ -79,7 +79,7 @@ void servo_angle(int angle, int motor)
   else if (motor == 4){
     PWM_Instance_6B->setPWM(MOTOR_4, Freq, value);}
 
-  else if (motor == 5){
+  /*else if (motor == 5){
     PWM_Instance_7A->setPWM(MOTOR_5, Freq, value);}
 
   else if (motor == 6){
@@ -89,5 +89,6 @@ void servo_angle(int angle, int motor)
     PWM_Instance_0A->setPWM(MOTOR_7, Freq, value);}
 
   else if (motor == 8){
-    PWM_Instance_0B->setPWM(MOTOR_8, Freq, value);}
+    PWM_Instance_0B->setPWM(MOTOR_8, Freq, value);}*/
+    
 }
