2d1e24d0b713a2381961f80097a11f527ecf7440
Diogo Vilela <diogoagoncalves2001@gmail.com>
2022-11-26 18:51:51 +0000

    boda boda boda

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,20 @@
 #define Ograus_Motor2 19.667
 #define Ograus_Motor3 19.333
 #define Ograus_Motor4 35.833
+#define Ograus_Motor5 21
+#define Ograus_Motor6 21
+#define Ograus_Motor7 21
+#define Ograus_Motor8 21
+
 
 RP2040_PWM *PWM_Instance_1A;
 RP2040_PWM *PWM_Instance_1B;
 RP2040_PWM *PWM_Instance_6A;
 RP2040_PWM *PWM_Instance_6B;
-/*RP2040_PWM *PWM_Instance_7A;
+RP2040_PWM *PWM_Instance_7A;
 RP2040_PWM *PWM_Instance_7B;
 RP2040_PWM *PWM_Instance_0A;
-RP2040_PWM *PWM_Instance_0B;*/
+RP2040_PWM *PWM_Instance_0B;
 
 void servo_angle(int angle, int motor);
 
@@ -39,59 +44,67 @@ void setup()
   PWM_Instance_6A = new RP2040_PWM(MOTOR_3, Freq, Ograus_Motor3);
   PWM_Instance_6B = new RP2040_PWM(MOTOR_4, Freq, Ograus_Motor4);
 
-  /*PWM_Instance_7A = new RP2040_PWM(MOTOR_5, Freq, Ograus);
-  PWM_Instance_7B = new RP2040_PWM(MOTOR_6, Freq, Ograus);
+  PWM_Instance_7A = new RP2040_PWM(MOTOR_5, Freq, Ograus_Motor5);
+  PWM_Instance_7B = new RP2040_PWM(MOTOR_6, Freq, Ograus_Motor6);
 
-  PWM_Instance_0A = new RP2040_PWM(MOTOR_7, Freq, Ograus);
-  PWM_Instance_0B = new RP2040_PWM(MOTOR_8, Freq, Ograus);*/
+  PWM_Instance_0A = new RP2040_PWM(MOTOR_7, Freq, Ograus_Motor7);
+  PWM_Instance_0B = new RP2040_PWM(MOTOR_8, Freq, Ograus_Motor8);
 }
 
 void loop()
 {
 
-  for (int i = 1; i < 5; i++)
-  {
-    
-    
-    if(i==2){
-      servo_angle(82, i);
-    }
-
-    else if(i==3){
-      servo_angle(80, i);
-    }
-
-    else if(i==1){
-      servo_angle(175, i);
-    }
-
-    else if(i==4){
-      servo_angle(179, i);
-    }
-
-
-    
-  }
-  delay(1000);
   
-  for (int i = 1; i < 5; i++)
-  {
-    if(i==1 ){
-      servo_angle(87, i);
-    }
+  servo_angle(0,1);
+  servo_angle(0,2);
+  servo_angle(0,3);
+  servo_angle(0,4);
 
-    else if(i==4){
-      servo_angle(90, i);
-    }
+  //para levantar o robot angulo menor
+  servo_angle(0,5);
+    
 
-    else if(i==2 ){
-      servo_angle(175, i);
-    }
+  //para levantar o robot angulo maior
+  servo_angle(180,6);
+    
 
-    else if( i==3){
-      servo_angle(170, i);
-    }
-  }
+  //para levantar o robot angulo maior
+  servo_angle(180,7);
+    
+
+  //para levantar o robot angulo menor
+  servo_angle(0,8);
+    
+
+
+    
+  
+  delay(1000);
+
+  
+
+  servo_angle(90,1);
+  servo_angle(90,2);
+  servo_angle(90,3);
+  servo_angle(90,4);
+
+  //para levantar o robot angulo menor
+  servo_angle(45,5);
+    
+
+  //para levantar o robot angulo maior
+  servo_angle(135,6);
+  
+
+  //para levantar o robot angulo maior
+  servo_angle(135,7);
+    
+
+  //para levantar o robot angulo menor
+  servo_angle(45,8);
+    
+
+  
   delay(1000);
 }
 
@@ -101,30 +114,45 @@ void servo_angle(int angle, int motor)
   if(angle < 0 || angle > 180){ return; }
 
   float value=0;
-  value = (0.16666666666666667) * angle + 6;
 
   if (motor == 1){
-    PWM_Instance_1A->setPWM(MOTOR_1, Freq, value);}
+    value = 0.1630 * angle + 20.50;
+    PWM_Instance_1A->setPWM(MOTOR_1, Freq, value);
+  }
 
   else if (motor == 2){
-    PWM_Instance_1B->setPWM(MOTOR_2, Freq, value);}
+    value = 35.17 -0.1722 * angle;
+    PWM_Instance_1B->setPWM(MOTOR_2, Freq, value);
+  }
 
   else if (motor == 3){
-    PWM_Instance_6A->setPWM(MOTOR_3, Freq, value);}
+    value = 34.33 - 0.1704 * angle;
+    PWM_Instance_6A->setPWM(MOTOR_3, Freq, value);
+  }
 
   else if (motor == 4){
-    PWM_Instance_6B->setPWM(MOTOR_4, Freq, value);}
+    value = 0.1630 * angle + 21.33;
+    PWM_Instance_6B->setPWM(MOTOR_4, Freq, value);
+  }
 
-  /*else if (motor == 5){
-    PWM_Instance_7A->setPWM(MOTOR_5, Freq, value);}
+  else if (motor == 5){
+    value = (0.16666666666666667) * angle + 6;
+    PWM_Instance_7A->setPWM(MOTOR_5, Freq, value);
+  }
 
   else if (motor == 6){
-    PWM_Instance_7B->setPWM(MOTOR_6, Freq, value);}
+    value = (0.16666666666666667) * angle + 6;
+    PWM_Instance_7B->setPWM(MOTOR_6, Freq, value);
+  }
 
   else if (motor == 7){
-    PWM_Instance_0A->setPWM(MOTOR_7, Freq, value);}
+    value = (0.16666666666666667) * angle + 6;
+    PWM_Instance_0A->setPWM(MOTOR_7, Freq, value);
+  }
 
   else if (motor == 8){
-    PWM_Instance_0B->setPWM(MOTOR_8, Freq, value);}*/
+    value = (0.16666666666666667) * angle + 6;
+    PWM_Instance_0B->setPWM(MOTOR_8, Freq, value);
+  }
     
 }
