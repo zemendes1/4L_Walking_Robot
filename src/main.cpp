91c3c88b4c123aa5c0648d77e66440d0a41d509d
Diogo Vilela <diogoagoncalves2001@gmail.com>
2022-11-30 21:34:19 +0000

    WYI FYI

--- src/main.cpp
+++ src/main.cpp
@@ -66,42 +66,45 @@ void standby();
 void liedown();
 void standup();
 void dancing();
+void conexao_html();
+void conecta_wifi();
 
 
 float m1= 0.000f,m2= 0.000f,m3= 0.000f,m4= 0.000f,m5= 0.000f,m6= 0.000f,m7= 0.000f,m8= 0.000f;
 float b1= 0.000f,b2= 0.000f,b3= 0.000f,b4= 0.000f,b5= 0.000f,b6= 0.000f,b7= 0.000f,b8= 0.000f;
 
+
+
+
 const char* ssid = "MEO-F44E90";
 const char* password = "de6ee7ea05";
+
+
+
+
+// Set web server port number to 80
+WiFiServer server(80);
+ 
+// Variable to store the HTTP request
+String header;
+ 
+// Variable to store onboard LED state
+String picoLEDState = "off";
+ 
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+
+
 
 void setup()
 {
   // put your setup code here, to run once:
-  //calibra();
-
-  Serial.begin(115200);
- 
-  // Operate in WiFi Station mode
-  WiFi.mode(WIFI_STA);
- 
-  // Start WiFi with supplied parameters
-  WiFi.begin(ssid, password);
- 
-  // Print periods on monitor while establishing connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    delay(500);
-  }
- 
-  // Connection established
-  Serial.println("");
-  Serial.print("Pico W is connected to WiFi network ");
-  Serial.println(WiFi.SSID());
- 
-  // Print IP Address
-  Serial.print("Assigned IP Address: ");
-  Serial.println(WiFi.localIP());
+  calibra();
 
   /*PWM_Instance_1A = new RP2040_PWM(MOTOR_1, Freq, _180graus_Motor1);
   PWM_Instance_1B = new RP2040_PWM(MOTOR_2, Freq, _90graus_Motor2);
@@ -114,7 +117,9 @@ void setup()
 
   PWM_Instance_0A = new RP2040_PWM(MOTOR_7, Freq, _90graus_Motor7);
   PWM_Instance_0B = new RP2040_PWM(MOTOR_8, Freq, _90graus_Motor8);*/
-  pinMode(6,OUTPUT);
+
+  conecta_wifi();
+  
 }
 
 void loop()
@@ -128,10 +133,7 @@ void loop()
 
   //PWM_Instance_1A->setPWM(MOTOR_1, Freq, 20.3);
 
-  digitalWrite(6,HIGH);
-  delay(1000);
-  digitalWrite(6,HIGH);
-  delay(1000);
+  conexao_html();
   
 }
 
@@ -289,4 +291,121 @@ void calibra()
   m8 = m8 / (180 - 90); 
   b8 = _90graus_Motor8 - (90*m8);  
   
+}
+
+void conexao_html (){
+
+  WiFiClient client = server.available();   // Listen for incoming clients
+ 
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+ 
+            // Switch the LED on and off
+            if (header.indexOf("GET /led/on") >= 0) {
+              Serial.println("LED on");
+              picoLEDState = "on";
+              digitalWrite(6, HIGH);
+            } else if (header.indexOf("GET /led/off") >= 0) {
+              Serial.println("LED off");
+              picoLEDState = "off";
+              digitalWrite(6, LOW);
+            }
+ 
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+ 
+            // CSS to style the on/off buttons
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #F23A3A;}</style></head>");
+ 
+            // Web Page Heading
+            client.println("<body><h1>Pico W LED Control</h1>");
+ 
+            // Display current state, and ON/OFF buttons for Onboard LED
+            client.println("<p>Onboard LED is " + picoLEDState + "</p>");
+            
+            // Set buttons
+            if (picoLEDState == "off") {
+              
+              //picoLEDState is off, display the ON button
+              client.println("<p><a href=\"/led/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+ 
+              //picoLEDState is on, display the OFF button
+              client.println("<p><a href=\"/led/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+ 
+            client.println("</body></html>");
+ 
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+
+}
+
+void conecta_wifi(){
+    // Start Serial Monitor
+  Serial.begin(115200);
+ 
+  // Initialize the LED as an output
+  pinMode(6, OUTPUT);
+ 
+  // Set LED off
+  digitalWrite(6, LOW);
+ 
+  // Connect to Wi-Fi network with SSID and password
+  WiFi.begin(ssid, password);
+ 
+  // Display progress on Serial monitor
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.print("WiFi connected at IP Address ");
+  Serial.println(WiFi.localIP());
+ 
+  // Start Server
+  server.begin();
 }
