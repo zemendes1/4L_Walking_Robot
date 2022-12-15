0dbe8d4204737b6f4b50f8dc14840ffbbea8311c
José Mendes <61622582+zemendes1@users.noreply.github.com>
2022-12-15 09:35:11 +0000

    Remove TOF

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#include <VL53L0X.h>
 #include <HCSR04.h>
 #include <MPU6050.h>
 
@@ -13,9 +12,6 @@
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 float distance_sonar=0.000f;
 
-//TOF
-VL53L0X tof;
-float distance_tof=0.000f, prev_distance_tof=0.000f;
 
 //MPU
 MPU6050	mpu;
@@ -34,51 +30,12 @@ void sonar_loop () {
 
 
 
-void tof_setup() {
- 
-  Wire.setSDA(sda_tof);//GPIO8
-  Wire.setSCL(scl_tof);//GPIO9
-
-  Wire.begin();
-
-
-  tof.setTimeout(500);
-  while (!tof.init()) {
-    Serial.println(F("Failed to detect and initialize VL53L0X!"));
-    delay(100);
-  }  
-
-  // Reduce timing budget to 20 ms (default is about 33 ms)
-  tof.setMeasurementTimingBudget(20000);
-
-  // Start new distance measure
-  tof.startReadRangeMillimeters();  
-
-}
-
-void tof_loop() {
-
-  if (tof.readRangeAvailable()) {
-    prev_distance_tof = distance_tof;
-    distance_tof = tof.readRangeMillimeters() * 0.1;
-
-     
-    // Start new distance measure
-    tof.startReadRangeMillimeters(); 
-
-    //Debug distance_tof
-    Serial.print("Distance_tof: ");
-    Serial.println(distance_tof, 3);
-
-  }
- 
-}
 
 //Utilizei um exemplo de gyroscope mas há mais opções (temperatura, aceleração, etc)
 void imu_setup() {
 
-  Wire.setSDA(7);//GPIO20
-  Wire.setSCL(6);//GPIO21
+  Wire.setSDA(sda_imu);//GPIO20
+  Wire.setSCL(scl_imu);//GPIO21
 
   Wire.begin();
   // Initialize MPU6050
--- src/sensor.h
+++ src/sensor.h
@@ -3,7 +3,7 @@
 /****/
 #include <Arduino.h>
 #include <Wire.h>
-#include <VL53L0X.h>
+
 #include <HCSR04.h>
 #include <MPU6050.h>
 
@@ -11,22 +11,15 @@
 #define triggerPin 27 
 #define echoPin 28
 
-//TOF
-#define sda_tof 8 
-#define scl_tof 9
-
 //MPU
-#define sda_imu 21 
-#define scl_imu 22
+#define sda_imu 8
+#define scl_imu 9
 
 extern float distance_sonar;
 extern float distance_tof, prev_distance_tof;
 
 void sonar_setup();
 void sonar_loop();
-
-void tof_setup();
-void tof_loop();
 
 void imu_setup();
 void imu_loop();
